c1e0d82cf628c9c76bc7f6b7430ae4880874f9c0
Cat <kaubariskis123@gmail.com>
2016-09-25 21:27:37 +0300

    added first part of update now connect DB and...
    ...other commands

--- DriftinBOTv2.js
+++ DriftinBOTv2.js
@@ -1,1 +1,64 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({bot: true});
 
+// SETTINGS
+const settings = {}
+settings.config = require('./config.json');
+settings.token = settings.config.token
+settings.prefix = `drift `
+settings.CatID = `177286767903244288`
+settings.DebossID = `160853902726660096`
+settings.autorestart = true
+settings.exec = require('child_process').exec;
+settings.pf = settings.prefix ? settings.prefix : `${client.user.toString()} `;
+// SETTINGS
+
+const commands = {
+    'git-pull': msg => {
+      if (msg.author.id === settings.CatID || msg.author.id === settings.DebossID) {
+settings.exec(`git pull`, function(err, stdout, stderr){
+      msg.channel.sendMessage("\`\`\`xl\n" + stdout +"\n"+ stderr + "\n\`\`\`").then(() => {if (settings.autorestart = true) return process.exit(1); else {return msg.channel.sendMessage(`you should restart bot now`)}})
+    });
+    }
+    },
+    'restart': msg => {
+        if (msg.author.id === settings.CatID || msg.author.id === settings.DebossID) {
+        msg.channel.sendMessage('initiating protocol RESTART DRIFTIN BOT').then(() => process.exit(1));
+    }  },
+    'eval': (msg, newContent) => {
+        if (msg.author.id === settings.CatID || msg.author.id === settings.DebossID) {
+        const code = msg.sbustring(11)
+        if (!code.length) return message.reply(`Add some code there`);
+        try {
+            const output = eval(code);
+            msg.channel.sendMessage(
+                `\`INPUT:\`\n\`\`\`${code.replace(/`/g, '"')}\`\`\`\n\`OUTPUT:\`\n\`\`\`${typeof output === 'object' ? JSON.stringify(output) : output}\`\`\``
+            );
+        } catch(err) {
+            msg.channel.sendMessage(`\`INPUT:\`\n\`\`\`${code.replace(/`/g, '"')}\`\`\`\n\`ERROR:\`\n\`\`\`${err}\`\`\``);
+        }
+    }
+  }
+};
+
+client.on('ready', () => {
+    console.log('Lets drift'); bot.user.setStatus(`online`, 'The Bot Prefix Is drift (drift stats list)')
+});
+
+client.on('reconnecting', () => {
+    console.log('my batteries ended need to change them and join the figth back');
+});
+
+client.on('message', msg => {
+
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(settings.pf)) return;
+
+    const content = msg.content.slice(settings.pf.length);
+
+    const command = commands[content.split(' ')[0]];
+    command(msg, content);
+});
+
+
+client.login(settings.token);
