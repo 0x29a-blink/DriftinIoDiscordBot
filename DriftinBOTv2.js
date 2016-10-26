f0318ca2cdaacb5c5d706d2634bc0722326eb5ce
DebossTFK <natethornton11@gmail.com>
2016-10-26 07:30:49 -0400

    Update DriftinBOTv2.js

--- DriftinBOTv2.js
+++ DriftinBOTv2.js
@@ -25,6 +25,7 @@ settings.cmdeasy = new Map([
 	["owner/catcontact", "Cat's Contact Info\nDiscordMention : @Cat#0822\nDiscord ID : 177286767903244288\n***Git/hub*** : https://github.com/Cat-who-loves-bots"],
 	["stats shuriken", "**Unavalibe** __No Provided Stats From Sidney (the game developer)__"],
 	["help", "DriftinBot Commands\n drift owner\n drift owner/debosscontact\n drift owner/catcontact\n drift official server\n drift secret class\n drift stats list"],
+	["maps", "**All current maps are**\nSquare\nCircle\n`Note : i do not know the real map names but i just nammed them based off of their shape`"],
         [`stats racer`, `**❯_ **Racer
 **❯_ **Speed❯${settings.DB.racer.Speed}
 **❯_ **Defense❯${settings.DB.racer.Defense}
